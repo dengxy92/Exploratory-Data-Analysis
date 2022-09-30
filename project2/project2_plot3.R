073c4156473be736605f0b6d9af665f328e2b739
dengxy92 <71602454+dengxy92@users.noreply.github.com>
2022-09-30 17:46:17 -0400

    Add files via upload

---
+++ project2/project2_plot3.R
@@ -0,0 +1,13 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+library(ggplot2)
+M_emission <- NEI[NEI$fips == "24510",]
+png(filename = "./project2plot3.png", width = 6, height = 5, units = 'in', res = 500)
+(ggplot(M_emission, aes(year, Emissions, fill = type))+geom_bar(stat="identity")+
+  facet_wrap(.~type, nrow=2, ncol=2) + ylab(expression('Total PM'[2.5]*" Emissions"))+
+ggtitle("Total Emissions in Baltimore City from 1999 to 2008") 
+  + scale_x_continuous(breaks = c(1998, 2002, 2005,2008)))
+
+
+dev.off()
